c46262c7dcce849a84a7785c926f99de4a5bede3
renzycode <renzyjohnm1@gmail.com>
2023-03-04 01:41:11 +0800

    update

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PHP Encryptor</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">    
+    <title>PHP hasher</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/vendor/bootstrap-icons/bootstrap-icons.css">
 </head>
 
@@ -23,25 +22,35 @@
 ?>
 
 <body>
-    <div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center align-self-center mt-5">
         <form action="index.php" class="row mt-5 shadow" method="post">
             <div class="col-12 border bg-dark border-1 border-dark">
-                <h4 class="text-light mt-2">PHP Encryptor</h4>
+                <h4 class="text-light mt-2">PHP hasher</h4>
             </div>
             <div class="col-12 border border-1 border-dark">
                 <div class="my-3">
                     <label class="form-label">Password</label>
-                    <input type="text" class="form-control border border-dark" name="pass" value="<?php echo $password ?>" required>
+                    <input type="text" class="form-control border border-dark" name="pass"
+                        value="<?php echo $password ?>" required>
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">Encrypted Password</label>
-                    <input type="text" class="form-control border border-dark" value="<?php echo $encrypted_password ?>">
+                    <label class="form-label">Hashed Password</label>
+                    <textarea rows="3" type="text" class="form-control border border-dark"
+                        ><?php echo $encrypted_password ?></textarea>
                 </div>
                 <div class="mb-3">
-                    <button class="btn btn-dark" type="submit">Encrypt</button>
+                    <button class="btn btn-dark" type="submit">Hash Hash</button>
                 </div>
             </div>
         </form>
+    </div>
+    <div class="card fixed-bottom">
+        <div class="card-body">
+            <blockquote class="blockquote my-2">
+                <footer class="blockquote-footer">© 2021 Copyright: RenzyCode</cite>
+                </footer>
+            </blockquote>
+        </div>
     </div>
     <script src="assets/js/bootstrap.min.js"></script>
 </body>
